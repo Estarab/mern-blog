6fbe6045abe900d982c66bf17396877139607c59
Estarab <abnationtech@gmail.com>
2024-08-15 11:42:26 +0200

    sign up api route

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
  import mongoose from 'mongoose';
  import dotenv from 'dotenv';
  import userRoutes from './routes/user.route.js';
-// import authRoutes from './routes/auth.route.js';
+ import authRoutes from './routes/auth.route.js';
 // import postRoutes from './routes/post.route.js';
 // import commentRoutes from './routes/comment.route.js';
 // import cookieParser from 'cookie-parser';
@@ -22,7 +22,7 @@
 
  const app = express();
 
-// app.use(express.json());
+ app.use(express.json());
 // app.use(cookieParser());
 
  app.listen(3000, () => {
@@ -30,7 +30,7 @@
  });
 
  app.use('/api/user', userRoutes);
-// app.use('/api/auth', authRoutes);
+ app.use('/api/auth', authRoutes);
 // app.use('/api/post', postRoutes);
 // app.use('/api/comment', commentRoutes);
 
