8eafcb226166f433f6ca64fd4295ef71b90381ac
Estarab <abnationtech@gmail.com>
2024-08-19 19:01:46 +0200

    slider imgs

--- client/src/pages/Programs.jsx
+++ client/src/pages/Programs.jsx
@@ -5,7 +5,7 @@ export default function Programs() {
     <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
       <h1 className='text-3xl font-semibold'>Programs</h1>
       <p className='text-md text-gray-500'>Download Our Mobile App</p>
-      <CallToAction />
+      {/* <CallToAction /> */}
     </div>
   )
 }
