a6dd7819e90a01e6c7640647dda7e1893b2aa0ab
Estarab <abnationtech@gmail.com>
2024-08-19 20:38:37 +0200

    link footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -205,7 +205,7 @@ export default function FooterCom() {
             <Footer.Icon href='#' icon={BsFacebook}/>
             <Footer.Icon href='#' icon={BsInstagram}/>
             <Footer.Icon href='#' icon={BsTwitter}/>
-            <Footer.Icon href='https://github.com/sahandghavidel' icon={BsGithub}/>
+            <Footer.Icon href='#' icon={BsGithub}/>
             <Footer.Icon href='#' icon={BsDribbble}/>
 
           </div>
