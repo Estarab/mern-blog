b57c0636836d5e97087769b8bc4d8e253572ac2f
Estarab <abnationtech@gmail.com>
2024-10-22 12:35:27 +0200

    w icon

--- client/src/components/WhatsAppIcon.jsx
+++ client/src/components/WhatsAppIcon.jsx
@@ -13,7 +13,7 @@ const WhatsAppIcon = () => {
       onClick={handleClick}
       className="fixed bottom-12 right-5 cursor-pointer z-50 flex flex-col items-center justify-center"
     >
-      <div className="bg-[#0A3D2D] shadow-lg rounded-full p-1 transition-transform duration-200 hover:scale-110 animate-bounce">
+      <div className="bg-[#0A3D2D] shadow-lg rounded-full p-1 transition-transform duration-200 hover:scale-110 animate-pulse">
         <FaWhatsapp size={18} color="#25D366" />
       </div>
       {/* <span className="mt-2 text-xs text-white text-center bg-[#0A3D2D] rounded px-2 py-1 shadow-md">
